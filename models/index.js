ca743ac173cd0e0f1e1f424004a7df5b2e24ec13
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-11-05 00:15:41 +0100

    Since we can't set 'console.log' as a value in config.json (since it's JSON and not full JavaScript), we need to set 'console.log' if 'logging' evaluates to true in models/index.js.

--- models/index.js
+++ models/index.js
@@ -6,6 +6,7 @@ var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + '/../config/config.json')[env];
+config.logging = config.logging ? console.log : false;
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db = {};
 
