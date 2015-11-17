8a2eeb10a35add78f5f4ec9aea18b6fe261aee18
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-11-17 22:19:53 +0100

    Rewrite the index spec so it actually works

--- specs/index-spec.js
+++ specs/index-spec.js
@@ -10,24 +10,24 @@ var request = require('supertest'),
     express = require('express'),
     db = require('./../models', {logging:console.log}),
     chai = require('chai'),
-    chaiAsPromised = require('chai-as-promised');
+    chaiAsPromised = require('chai-as-promised'),
+    expect = chai.expect,
+    server = require('../app.js');
 
-var expect = chai.expect;
+describe('GET /', function() {
+  // Create a fresh server instance prior to each test
+  beforeEach(function() {
+    this.app = server.setup(express());
+  });
 
-var app = require('../app.js');
-
-describe("GET", function() {
-    it('responds with the drywall admin index HTML', function() {
-    	try {
-	        request(app)
-	            .get('/')
-	            .set('Accept', 'text/html')
-	            .expect(200)
-	            .end(function(err, res) {
-	                expect(res.text).to.contain('Your Node.js website and user system is running');
-	            });
-    	} catch (error) {
-    		console.log("ERROR: " + JSON.stringify(error));
-    	}
+  it('responds with the drywall index HTML', function(done) {
+    request(this.app)
+      .get('/')
+      .set('Accept', 'text/html')
+      .expect(200)
+      .end(function(err, res) {
+        expect(res.text).to.contain('Your Node.js website and user system is running');
+        done();
+      });
     });
 });
