deb7d918ecd7778943d71c18e3bb6cbe7441db8b
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2015-11-17 22:19:31 +0100

    Move app setup into an exported function that can be invoked in tests

--- app.js
+++ app.js
@@ -17,82 +17,86 @@ var config = require('./config'),
     SequelizeStore = require('connect-session-sequelize')(session.Store),
     sequelizeStore = new SequelizeStore({ db: models.sequelize });
 
-sequelizeStore.sync({ force: dbConfig.force });
+exports.setup = function(app) {
+  sequelizeStore.sync({ force: dbConfig.force });
 
-//create express app
-var app = express();
+  //keep reference to config
+  app.config = config;
 
-//keep reference to config
-app.config = config;
+  //setup the web server
+  app.server = http.createServer(app);
+  app.db = models;
 
-//setup the web server
-app.server = http.createServer(app);
-app.db = models;
+  //settings
+  app.disable('x-powered-by');
+  app.set('port', config.port);
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
 
-//settings
-app.disable('x-powered-by');
-app.set('port', config.port);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+  // middleware
+  app.use(require('morgan')('dev'));
+  app.use(require('compression')());
+  app.use(require('serve-static')(path.join(__dirname, 'public')));
+  app.use(require('method-override')());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cookieParser(config.cryptoKey));
+  app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: config.cryptoKey,
+    store: sequelizeStore,
+    name: 'piratpartiet.sid',
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(csrf({ cookie: { signed: true } }));
+  helmet(app);
 
-// middleware
-app.use(require('morgan')('dev'));
-app.use(require('compression')());
-app.use(require('serve-static')(path.join(__dirname, 'public')));
-app.use(require('method-override')());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser(config.cryptoKey));
-app.use(session({
-  resave: true,
-  saveUninitialized: true,
-  secret: config.cryptoKey,
-  store: sequelizeStore,
-  name: 'piratpartiet.sid',
-  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(csrf({ cookie: { signed: true } }));
-helmet(app);
+  //response locals
+  app.use(function(req, res, next) {
+    res.cookie('_csrfToken', req.csrfToken());
+    res.locals = res.locals ? res.locals : {};
+    res.locals.user = {};
+    res.locals.user.defaultReturnUrl = req.user && req.user.defaultReturnUrl();
+    res.locals.user.username = req.user && req.user.username;
+    next();
+  });
 
-//response locals
-app.use(function(req, res, next) {
-  res.cookie('_csrfToken', req.csrfToken());
-  res.locals = res.locals ? res.locals : {};
-  res.locals.user = {};
-  res.locals.user.defaultReturnUrl = req.user && req.user.defaultReturnUrl();
-  res.locals.user.username = req.user && req.user.username;
-  next();
-});
+  // https://github.com/ethanl/connect-browser-logger
+  // app.use(require('browser-logger')());
 
-// https://github.com/ethanl/connect-browser-logger
-// app.use(require('browser-logger')());
+  //global locals
+  app.locals.projectName = app.config.projectName;
+  app.locals.copyrightYear = new Date().getFullYear();
+  app.locals.copyrightName = app.config.companyName;
+  app.locals.cacheBreaker = 'br34k-01';
 
-//global locals
-app.locals.projectName = app.config.projectName;
-app.locals.copyrightYear = new Date().getFullYear();
-app.locals.copyrightName = app.config.companyName;
-app.locals.cacheBreaker = 'br34k-01';
+  //setup passport
+  require('./passport')(app, passport);
 
-//setup passport
-require('./passport')(app, passport);
+  //setup routes
+  require('./routes')(app, passport);
 
-//setup routes
-require('./routes')(app, passport);
+  //custom (friendly) error handler
+  app.use(require('./views/http/index').http500);
 
-//custom (friendly) error handler
-app.use(require('./views/http/index').http500);
+  //setup utilities
+  app.utility = {
+      sendmail: require('./util/sendmail'),
+      slugify: require('./util/slugify'),
+      workflow: require('./util/workflow')
+  };
 
-//setup utilities
-app.utility = {
-    sendmail: require('./util/sendmail'),
-    slugify: require('./util/slugify'),
-    workflow: require('./util/workflow')
+  return app;
 };
 
-//listen up
-app.server.listen(app.config.port, function() {
+// create express app
+exports.app = this.setup(express());
+
+// listen up
+exports.app.server.listen(exports.app.config.port, function() {
   //and... we're live
   console.log('Server is running on port ' + config.port);
 });
