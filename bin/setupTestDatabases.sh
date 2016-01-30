a078e684d486ce6ea3916a5b52daa06fd919aad3
Mark Nyon <mark@minerapp.com>
2016-01-29 22:29:09 -0500

    Manually create database; #10

--- bin/setupTestDatabases.sh
+++ bin/setupTestDatabases.sh
@@ -3,4 +3,6 @@
 cp config/config.json.tmpl config/config.json 	 # Copy template file to template
 sed -i -- 's/"_INSERT USERNAME_"/"postgres"/g' config/config.json # change username for db
 sed -i -- 's/"_INSERT PASSWORD_"/""/g' config/config.json # change password for db
+if [ '$DB' = 'pgsql' ]; then psql -c 'DROP DATABASE IF EXISTS drywall-test;' -U postgres; fi
+if [ '$DB' = 'pgsql' ]; then psql -c 'create database drywall-test;' -U postgres; fi	
 cat config/config.json
