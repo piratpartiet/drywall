739c312a91a5b4dfc58eab9b00cbc80b6a4c8205
Mark Nyon <mark@minerapp.com>
2016-01-29 22:13:51 -0500

    Changed echo to cat #10

--- bin/setupTestDatabases.sh
+++ bin/setupTestDatabases.sh
@@ -3,4 +3,4 @@
 cp config/config.json.tmpl config/config.json 	 # Copy template file to template
 sed -i -- 's/"_INSERT USERNAME_"/postgres/g' config/config.json # change username for db
 sed -i -- 's/"_INSERT PASSWORD_"//g' config/config.json # change password for db
-echo config/config.json
+cat config/config.json
