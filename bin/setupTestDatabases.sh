cfbbc8e5d8e37cbde85ded74cf58eca4e3d18c9c
Mark Nyon <mark@minerapp.com>
2016-01-29 23:32:16 -0500

    Added istanbul & configured for test coverage; #20
    - bin/setupTestDatabases.sh - Added codeclimate repo token to capture
      lcov information from istanbul
    
    - package.json - added codeclimate-test-reporter and istanbul packages
      to the repo

--- bin/setupTestDatabases.sh
+++ bin/setupTestDatabases.sh
@@ -3,6 +3,8 @@
 cp config/config.json.tmpl config/config.json 	 # Copy template file to template
 sed -i -- 's/"_INSERT USERNAME_"/"postgres"/g' config/config.json # change username for db
 sed -i -- 's/"_INSERT PASSWORD_"/""/g' config/config.json # change password for db
+# Drop and create the test database
 if [ '$DB' = 'pgsql' ]; then psql -c 'DROP DATABASE IF EXISTS drywall-test;' -U postgres; fi
 if [ '$DB' = 'pgsql' ]; then psql -c 'create database drywall-test;' -U postgres; fi	
-cat config/config.json
+# set up Codeclimate to be run on the codebase
+CODECLIMATE_REPO_TOKEN=d59a2529d82564323b652ad2e84bfc4b477270f97207045fd7cd03d38d50ef22 codeclimate-test-reporter < coverage/lcov.info
