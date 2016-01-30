da902a65ab7b584555a6194dc2d653afac0bda2e
Mark Nyon <mark@minerapp.com>
2016-01-29 22:15:59 -0500

    Quoted replacement values
    - bin/setupTestDatabases.sh - Updated sed commands to quote replacement
      values.

--- bin/setupTestDatabases.sh
+++ bin/setupTestDatabases.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Script used to create & set up the test database for Continuous Integration
 cp config/config.json.tmpl config/config.json 	 # Copy template file to template
-sed -i -- 's/"_INSERT USERNAME_"/postgres/g' config/config.json # change username for db
-sed -i -- 's/"_INSERT PASSWORD_"//g' config/config.json # change password for db
+sed -i -- 's/"_INSERT USERNAME_"/"postgres"/g' config/config.json # change username for db
+sed -i -- 's/"_INSERT PASSWORD_"/""/g' config/config.json # change password for db
 cat config/config.json
