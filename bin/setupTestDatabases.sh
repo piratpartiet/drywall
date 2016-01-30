4e7b830c603b84d401cb50aaf0ca9083d53a26d5
Mark Nyon <mark@minerapp.com>
2016-01-29 22:06:35 -0500

    Continuous Integration Database Setup; #10
    - bin/setupTestDatabases.sh - Copy config.json.tmpl to config.json
      create a test database and update the username & password database to
      match the newly created database.

--- bin/setupTestDatabases.sh
+++ bin/setupTestDatabases.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-#
-#
-#cp config/config.json.tmpl config/config.json
-echo "Running Test Database Script"
+# Script used to create & set up the test database for Continuous Integration
+psql -c 'create database drywall-test;' -U postgres
+cp config/config.json.tmpl config/config.json 	 # Copy template file to template
+sed -i -- 's/"_INSERT USERNAME_"/postgres/g' config/config.json # change username for db
+sed -i -- 's/"_INSERT PASSWORD_"//g' config/config.json # change password for db
+#echo "Running Test Database Script"
